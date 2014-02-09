9a72cd1ef8028966ef19f4f035ad0954be7b4c02
Dahua Lin <lindahua@gmail.com>
2014-02-08 21:05:24 -0600

    minor changes

--- test/perfeval.jl
+++ test/perfeval.jl
@@ -37,18 +37,18 @@ r = ROCNums{Int}(
 @test precision(r) == (8/13)
 @test_approx_eq f1score(r) harmmean([recall(r), precision(r)])
 
+gt = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
+pr = [0, 0, 1, 0, 0, 1, 1, 0, 1, 2, 2, 2, 2, 0, 1]
+
+r0 = ROCNums{Int}(10, 5, 6, 4, 1, 2)
+@test rocnums(gt, pr) == r0
+
 gt = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
 ss = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1]
 
 @test rocnums(gt, ss, 0.25) == ROCNums{Int}(6, 5, 6, 2, 3, 0)
 @test rocnums(gt, ss, 0.55) == ROCNums{Int}(6, 5, 6, 5, 0, 0)
 @test rocnums(gt, ss, 0.75) == ROCNums{Int}(6, 5, 4, 5, 0, 2)
-
-gt = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
-pr = [0, 0, 1, 0, 0, 1, 1, 0, 1, 2, 2, 2, 2, 0, 1]
-
-r0 = ROCNums{Int}(10, 5, 6, 4, 1, 2)
-@test rocnums(gt, pr) == r0
 
 gt = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
 pr = [1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1]
