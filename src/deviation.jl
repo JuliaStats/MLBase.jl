b622371d65b761d714600eb104ecd30e439aad06
Dahua Lin <lindahua@gmail.com>
2014-02-10 09:37:24 -0600

    add src/deviation.jl

---
+++ src/deviation.jl
@@ -0,0 +1,92 @@
+# compute deviation between two arrays in different ways
+
+# squared L2 distance
+function sqL2dist{T<:Number}(a::ContiguousArray{T}, b::ContiguousArray{T})
+    n = length(a)
+    length(b) == n || throw(DimensionMismatch("Input dimension mismatch"))
+    r = 0.0
+    for i = 1:n
+        @inbounds r += abs2(a[i] - b[i]) 
+    end
+    return r
+end
+
+# L2 distance
+L2dist{T<:Number}(a::ContiguousArray{T}, b::ContiguousArray{T}) = sqrt(sqL2dist(a, b))
+
+# L1 distance
+function L1dist{T<:Number}(a::ContiguousArray{T}, b::ContiguousArray{T})
+    n = length(a)
+    length(b) == n || throw(DimensionMismatch("Input dimension mismatch"))
+    r = 0.0
+    for i = 1:n
+        @inbounds r += abs(a[i] - b[i]) 
+    end
+    return r    
+end
+
+# Linf distance
+function Linfdist{T<:Number}(a::ContiguousArray{T}, b::ContiguousArray{T})
+    n = length(a)
+    length(b) == n || throw(DimensionMismatch("Input dimension mismatch"))
+    r = 0.0
+    for i = 1:n
+        @inbounds a = abs(a[i] - b[i]) 
+        if r < a
+            r = a
+        end
+    end
+    return r     
+end
+
+# KL-divergence
+function _kldiv{T<:FloatingPoint}(a::ContiguousArray{T}, b::ContiguousArray{T})
+    n = length(a)
+    r = 0.0
+    for i = 1:n
+        @inbounds ai = a[i]
+        @inbounds bi = b[i]
+        if ai > 0
+            r += (ai * log(ai / bi) - ai + bi)
+        else
+            r += bi
+        end
+    end
+    return r::Float64
+end
+
+function kldiv{T<:FloatingPoint}(a::ContiguousVector{T}, b::ContiguousVector{T})
+    length(a) == length(b) || throw(DimensionMismatch("Input dimension mismatch"))
+    _kldiv(a, b)
+end
+
+function sumkldiv{T<:FloatingPoint}(a::ContiguousMatrix{T}, b::ContiguousMatrix{T})
+    size(a) == size(b) || throw(DimensionMismatch("Input dimension mismatch"))
+    _kldiv(a, b)
+end
+
+meankldiv{T<:FloatingPoint}(a::ContiguousMatrix{T}, b::ContiguousMatrix{T}) = sumkldiv(a, b) / size(a,2)
+
+
+# MeanAD: mean absolute deviation
+meanad{T<:Number}(a::ContiguousArray{T}, b::ContiguousArray{T}) = L1dist(a, b) / length(a)
+
+# MaxAD: maximum absolute deviation
+maxad{T<:Number}(a::ContiguousArray{T}, b::ContiguousArray{T}) = Linfdist(a, b)
+
+# MSD: mean squared deviation
+msd{T<:Number}(a::ContiguousArray{T}, b::ContiguousArray{T}) = sqL2dist(a, b) / length(a)
+
+# RMSD: root mean squared deviation
+rmsd{T<:Number}(a::ContiguousArray{T}, b::ContiguousArray{T}) = sqrt(msd(a, b))
+
+# NRMSD: normalized mean squared deviation
+function nrmsd{T<:Number}(a:ContiguousArray{T}, b::ContiguousArray{T})
+    amin, amax = extrema(a)
+    rmsd(a, b) / (amax - amin)
+end
+
+# PSNR: peak signal-to-noise ratio
+function psnr{T<:Real}(a::ContiguousArray{T}, b::ContiguousArray{T}, maxv::Real)
+    20. * log10(maxv) - 10. * log10(msd(a, b))
+end
