c9bd05a7dd74b69f392b8f363706bb43c3718a12
Anand Bisen <asbisen@icloud.com>
2018-09-04 04:56:27 +0000

    Fixed test dataset for a new class 0

--- test/perfeval.jl
+++ test/perfeval.jl
@@ -15,7 +15,7 @@ b = [1, 1, 2, 2, 2, 3, 3, 3]
 
 ## confusmat
 
-@test confusmat(a, b) == [2 1 0; 0 2 1; 0 0 2]
+@test confusmat(a, b) == [0 1 0 0; 0 1 1 0; 0 0 2 1; 0 0 0 2]
 
 ## counthits & hitrates
 
